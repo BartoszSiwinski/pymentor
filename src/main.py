1179221e0a6f79149c63d68eddfce48bb9db2d7d
Bartosz Siwinski <bartsiwinski@gmail.com>
2024-01-21 16:52:09 +0100

    Add module for saving results to excel. Name refactor.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
-
 from read_raw_data import get_processed_data_for_mentoring_program
 from matching import get_mentor_mentee_pairs, get_mentors_and_mentees
+from save_results import save_mentors_to_excel, save_mentees_to_excel, save_mentorship_pairs_excel
 
 
 def main():
@@ -10,20 +10,28 @@ def main():
     )
 
     mentors, mentees = get_mentors_and_mentees(participants)
-    results = get_mentor_mentee_pairs(mentors, mentees)
+    mentoring_pairs = get_mentor_mentee_pairs(mentors, mentees)
 
-    for mentor, mentee in results:
-        print(mentor.person.email_address, mentor.expertise.keys())
-        print(mentee.person.email_address, mentee.interests.keys())
-        print()
-
-    assigned_mentees = [x[1] for x in results]
+    assigned_mentees = [x[1] for x in mentoring_pairs]
     unassigned_mentees = [x for x in mentees if x not in assigned_mentees]
-    assigned_mentors = [x[0] for x in results]
+    assigned_mentors = [x[0] for x in mentoring_pairs]
     unassigned_mentors = [x for x in mentors if x not in assigned_mentors]
 
-    print([(mentor.person.email_address, mentor.expertise.keys()) for mentor in unassigned_mentors])
-    print([(mentee.person.email_address, mentee.interests.keys()) for mentee in unassigned_mentees])
+    save_mentors_to_excel(
+        '../results.xlsx',
+        'all_mentors',
+        assigned_mentors, unassigned_mentors
+    )
+    save_mentees_to_excel(
+        '../results.xlsx',
+        'all_mentees',
+        assigned_mentees, unassigned_mentees
+    )
+    save_mentorship_pairs_excel(
+        '../results.xlsx',
+        'mentoring_pairs',
+        mentoring_pairs
+    )
 
 
 if __name__ == '__main__':
---
+++ src/save_results.py
@@ -0,0 +1,83 @@
+import os
+
+import pandas as pd
+
+from data_classes import Mentee, Mentor
+
+
+def _append_data_to_excel(
+        excel_file_path: str,
+        excel_sheet_name: str,
+        df: pd.DataFrame
+):
+    excel_file_exists = os.path.isfile(excel_file_path)
+
+    with pd.ExcelWriter(
+            excel_file_path,
+            engine='openpyxl',
+            mode='a' if excel_file_exists else 'w',
+            if_sheet_exists='replace' if excel_file_exists else None
+    ) as writer:
+        df.to_excel(writer, excel_sheet_name, index=False, engine='openpyxl')
+
+
+def save_mentors_to_excel(
+        excel_file_path: str,
+        excel_sheet_name: str,
+        assigned_mentors: list[Mentor],
+        unassigned_mentors: list[Mentor]
+):
+    df = pd.DataFrame(
+        [
+            {
+                "email_address": mentor.person.email_address,
+                "fullname": mentor.person.fullname,
+                "years_of_experience": mentor.person.years_of_experience,
+                "business_unit": mentor.person.business_unit,
+                "assigned": mentor in assigned_mentors,
+                "expertise": list(mentor.expertise.keys())
+            } for mentor in assigned_mentors + unassigned_mentors
+        ]
+    )
+    _append_data_to_excel(excel_file_path, excel_sheet_name, df)
+
+
+def save_mentees_to_excel(
+        excel_file_path: str,
+        excel_sheet_name: str,
+        assigned_mentees: list[Mentee],
+        unassigned_mentees: list[Mentee]
+):
+    df = pd.DataFrame(
+        [
+            {
+                "email_address": mentee.person.email_address,
+                "fullname": mentee.person.fullname,
+                "years_of_experience": mentee.person.years_of_experience,
+                "business_unit": mentee.person.business_unit,
+                "assigned": mentee in assigned_mentees,
+                "interests": list(mentee.interests.keys())
+            } for mentee in assigned_mentees + unassigned_mentees
+        ]
+    )
+    _append_data_to_excel(excel_file_path, excel_sheet_name, df)
+
+
+def save_mentorship_pairs_excel(
+        excel_file_path: str,
+        excel_sheet_name: str,
+        pairs: list[tuple[Mentor, Mentee]]
+):
+    df = pd.DataFrame(
+        [
+            {
+                "email_address_mentor": mentor.person.email_address,
+                "fullname_mentor": mentor.person.fullname,
+                "mentor_expertise": list(mentor.expertise.keys()),
+                "email_address_mentee": mentee.person.email_address,
+                "fullname_mentee": mentee.person.fullname,
+                "mentee_interests": list(mentee.interests.keys())
+            } for mentor, mentee in pairs
+        ]
+    )
+    _append_data_to_excel(excel_file_path, excel_sheet_name, df)
